0461594efc1047f26623ff2574a79a19ee42d64f
degita <degita@degita-dynabook.(none)>
2014-01-19 15:47:19 +0900

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonTbs::Application.config.secret_token = '05f4865e5e4d716da7409b8e76f9ba73bea422a4421a142f03975e6fc6f2d7b88a6fe2df73ac157cf887688c7536ee0414e77b99595396aa696fe09e9014646a'
